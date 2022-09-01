090d3ba01e800f7fd016a0960ceab2329099170f
Jsing <joosing711@gmail.com>
2022-09-02 06:45:24 +0900

    메인함수를 가지는 클래스를 직접 생성하고 "Hello World"를 찍습니다.

---
+++ src/main/java/SpringMain.java
@@ -0,0 +1,7 @@
+import java.sql.SQLException;
+
+public final class SpringMain {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println("hello");
+    }
+}
