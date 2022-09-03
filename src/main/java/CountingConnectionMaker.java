7d140bd4e8161795fd33dcb8872f8cd1ed1072ac
Jsing <joosing711@gmail.com>
2022-09-03 21:51:25 +0900

    연결횟수 카운팅 기능이 있는 DB 연결 클래스

---
+++ src/main/java/CountingConnectionMaker.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CountingConnectionMaker implements ConnectionMaker {
+    int counter;
+    private ConnectionMaker realConnectionMaker;
+
+    public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+        counter++;
+        return realConnectionMaker.makeConnection();
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
