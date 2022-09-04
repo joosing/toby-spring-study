4e4ad160a9d3157e39dda52174625d912205b1dc
Jsing <joosing711@gmail.com>
2022-09-04 10:24:19 +0900

    XML을 통해 의존관계 주입에 필요한 디폴트 생성자 및 Setter 정의

--- src/main/java/CountingConnectionMaker.java
+++ src/main/java/CountingConnectionMaker.java
@@ -5,7 +5,13 @@ public class CountingConnectionMaker implements ConnectionMaker {
     int counter;
     private ConnectionMaker realConnectionMaker;
 
+    public CountingConnectionMaker() {}
+
     public CountingConnectionMaker(ConnectionMaker realConnectionMaker) {
+        this.realConnectionMaker = realConnectionMaker;
+    }
+
+    public void setRealConnectionMaker(ConnectionMaker realConnectionMaker) {
         this.realConnectionMaker = realConnectionMaker;
     }
 
--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -4,9 +4,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private ConnectionMaker connectionMaker;
+
+    public UserDao() {}
 
     public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
