4ea2198b513fd0869415442adc0ef54928ab4bd2
Jsing <joosing711@gmail.com>
2022-09-04 10:25:00 +0900

    XML 설정을 사용해 어플리케이션 컨텍스트를 생성할 별도의 main 정의

---
+++ src/main/java/UserDaoTestWithXml.java
@@ -0,0 +1,32 @@
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class UserDaoTestWithXml {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        final ApplicationContext applicationContext =
+                new GenericXmlApplicationContext("applicationContext.xml");
+        final UserDao dao = applicationContext.getBean("userDao", UserDao.class);
+
+        final User user = new User();
+        user.setId("JooSing");
+        user.setName("주승");
+        user.setPassword("hello");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        final User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+
+        final CountingConnectionMaker ccm = applicationContext.getBean("connectionMaker",
+                                                                       CountingConnectionMaker.class);
+        System.out.println("Connection counter : " + ccm.getCounter());
+    }
+}
