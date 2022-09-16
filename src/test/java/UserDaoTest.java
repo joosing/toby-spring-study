df5ebf5c9e3cc9a1e653e16abd182d25cfb71840
Jsing <joosing711@gmail.com>
2022-09-17 07:55:26 +0900

    getCount() 테스트

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -4,12 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
     @Test
     public void addAndGet() throws Exception {
         final ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(DaoFactory.class);
+                new GenericXmlApplicationContext("applicationContext.xml");
         final UserDao dao = applicationContext.getBean("userDao", UserDao.class);
 
         dao.deleteAll();
@@ -26,5 +27,28 @@ public class UserDaoTest {
         final User testUser = dao.get(expectedUser.getId());
         Assert.assertEquals(expectedUser.getName(), testUser.getName());
         Assert.assertEquals(expectedUser.getId(), testUser.getId());
+    }
+
+    @Test
+    public void getCount() throws Exception {
+        final ApplicationContext applicationContext =
+                new GenericXmlApplicationContext("applicationContext.xml");
+        final UserDao dao = applicationContext.getBean("userDao", UserDao.class);
+
+        final User expectedUser1 = new User("joosing", "한주승", "hello");
+        final User expectedUser2 = new User("hey", "헤이", "hey");
+        final User expectedUser3 = new User("hi", "하이", "hi");
+
+        dao.deleteAll();
+        Assert.assertEquals(0, dao.getCount());
+
+        dao.add(expectedUser1);
+        Assert.assertEquals(1, dao.getCount());
+
+        dao.add(expectedUser2);
+        Assert.assertEquals(2, dao.getCount());
+
+        dao.add(expectedUser3);
+        Assert.assertEquals(3, dao.getCount());
     }
 }
