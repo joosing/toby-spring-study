d36398a9aa4d7f4bc64356fc62093b710d4f11d8
Jsing <joosing711@gmail.com>
2022-09-17 07:41:52 +0900

    deleteAll()과 getCount() 테스트

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -12,12 +12,16 @@ public class UserDaoTest {
                 new AnnotationConfigApplicationContext(DaoFactory.class);
         final UserDao dao = applicationContext.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        Assert.assertEquals(0, dao.getCount());
+
         final User expectedUser = new User();
         expectedUser.setId("JooSing");
         expectedUser.setName("주승");
         expectedUser.setPassword("hello");
 
         dao.add(expectedUser);
+        Assert.assertEquals(1, dao.getCount());
 
         final User testUser = dao.get(expectedUser.getId());
         Assert.assertEquals(expectedUser.getName(), testUser.getName());
