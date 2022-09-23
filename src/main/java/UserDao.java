02c1032dc99723f4ae55a73a87c8dbd51e56d397
Jsing <joosing711@gmail.com>
2022-09-23 20:14:18 +0900

    부가정보가 있는 AddStatement를 처리하고 UserDao.add() 메서드 리팩토링

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -22,15 +22,8 @@ public class UserDao {
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        try (Connection c = dataSource.getConnection();
-             PreparedStatement ps = c.prepareStatement(
-                "insert into users(id, name, password) values(?,?,?)")
-             ){
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-            ps.executeUpdate();
-        }
+        final StatementStrategy strategy = new AddStatement(user);
+        jdbcContextWithStatementStrategy(strategy);
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
