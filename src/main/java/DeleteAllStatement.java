803af8be27664dfece0ec3dbf09976e9a9cc4e8a
Jsing <joosing711@gmail.com>
2022-09-23 09:26:43 +0900

    DeleteAll 전략 객체 정의

---
+++ src/main/java/DeleteAllStatement.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteAllStatement implements StatementStrategy {
+    @Override
+    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        return c.prepareStatement("delete from users");
+    }
+}
