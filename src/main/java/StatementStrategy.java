de575d4b8d3a0f86b75708c467acd54fc6c749cc
Jsing <joosing711@gmail.com>
2022-09-23 09:26:28 +0900

    전략 패턴에 적용할 전략 객체 인터페이스 정의

---
+++ src/main/java/StatementStrategy.java
@@ -0,0 +1,8 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
