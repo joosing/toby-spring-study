ac12ea8f478483b3ed4c82f681915ab0c02e07c2
Jsing <joosing711@gmail.com>
2022-09-24 11:55:45 +0900

    NonNull 처리추가

--- src/test/java/CalcSumTest.java
+++ src/test/java/CalcSumTest.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Objects;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,21 +8,22 @@ public class CalcSumTest {
     @Test
     public void sumOfNumbers() throws IOException {
         final Calculator calculator = new Calculator();
-        final int sum = calculator.calSum(getClass().getResource("numbers.txt").getPath());
+        final int sum = calculator.calSum(Objects.requireNonNull(getClass().getResource("numbers.txt")).getPath());
         Assert.assertEquals(10, sum);
     }
 
     @Test
     public void multiplyOfNumbers() throws IOException {
         final Calculator calculator = new Calculator();
-        final int sum = calculator.calcMultiply(getClass().getResource("numbers.txt").getPath());
+        final int sum = calculator.calcMultiply(Objects.requireNonNull(getClass().getResource("numbers.txt")).getPath());
         Assert.assertEquals(24, sum);
     }
 
     @Test
     public void concatenateStrings() throws IOException {
         final Calculator calculator = new Calculator();
-        final String result = calculator.concatenate(getClass().getResource("numbers.txt").getPath());
+        final String result = calculator.concatenate(
+                Objects.requireNonNull(getClass().getResource("numbers.txt")).getPath());
         Assert.assertEquals("1234", result);
     }
 }
