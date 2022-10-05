4179fb0ac2ce88df2085fda6cde785bbf638ca4b
Jsing <joosing711@gmail.com>
2022-10-06 08:21:09 +0900

    사용자 필드 추가 > User 필드 추가

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,40 +1,17 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
     private String id;
     private String name;
     private String password;
-
-    public User() {
-
-    }
-
-    public User(String id, String name, String password) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private Level level;
+    private int login;
+    private int recommand;
 }
