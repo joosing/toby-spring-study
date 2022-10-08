f1f5a32cd8cb719de121f89c9782c290f4811458
Jsing <joosing711@gmail.com>
2022-10-08 18:01:30 +0900

    사용자 레벨 업그레이드 정책 분리하기

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -2,36 +2,23 @@ import java.util.List;
 
 public class UserService {
     private UserDao userDao;
-
-    public static final int MIN_LOGIN_COUNT_FOR_SILVER = 50;
-    public static final int MIN_RECOMMEND_COUNT_FOR_GOLD = 30;
+    private UserLevelUpgradePolicy userLevelUpgradePolicy;
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
 
+    public void setUserLevelUpgradePolicy(UserLevelUpgradePolicy userLevelUpgradePolicy) {
+        this.userLevelUpgradePolicy = userLevelUpgradePolicy;
+    }
+
     public void upgradeLevels() {
         final List<User> users = userDao.getAll();
         for (User user : users) {
-            if (canUpgradeLevel(user)) {
-                upgradeLevel(user);
+            if (userLevelUpgradePolicy.can(user)) {
+                userLevelUpgradePolicy.upgrade(user);
             }
         }
-    }
-
-    private static boolean canUpgradeLevel(User user) {
-        return switch (user.getLevel()) {
-            case BASIC -> user.getLogin() >= MIN_LOGIN_COUNT_FOR_SILVER;
-            case SILVER -> user.getRecommend() >= MIN_RECOMMEND_COUNT_FOR_GOLD;
-            case GOLD -> false;
-            // default -> throw new IllegalArgumentException("Unknown Level: " + currentLevel);
-            // 위 default 문은 새로운 Switch 문에서 불필요합니다. Enum 값이 추가되었는데 case 가 추가되지 않으면 컴파일 오류가 발생합니다.
-        };
-    }
-
-    private void upgradeLevel(User user) {
-        user.upgradeLevel(); // User 객체의 레벨을 업그레이드 하고,
-        userDao.update(user); // DB에 있는 User 정보를 업그레이드 합니다. 와 간결하다!!!
     }
 
     public void add(User user) {
--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -24,10 +24,10 @@ public class UserServiceTest {
         userDao.deleteAll();
 
         users = Arrays.asList(
-                new User("bumjini", "박범진", "p1", Level.BASIC, UserService.MIN_LOGIN_COUNT_FOR_SILVER - 1, 0),
-                new User("joytouch", "강명성", "p2", Level.BASIC, UserService.MIN_LOGIN_COUNT_FOR_SILVER, 0),
-                new User("erwins", "신승한", "p3", Level.SILVER, 60, UserService.MIN_RECOMMEND_COUNT_FOR_GOLD-1),
-                new User("madnite1", "이상호", "p4", Level.SILVER, 60, UserService.MIN_RECOMMEND_COUNT_FOR_GOLD),
+                new User("bumjini", "박범진", "p1", Level.BASIC, GeneralUserLevelUpgradePolicy.MIN_LOGIN_COUNT_FOR_SILVER - 1, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, GeneralUserLevelUpgradePolicy.MIN_LOGIN_COUNT_FOR_SILVER, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, GeneralUserLevelUpgradePolicy.MIN_RECOMMEND_COUNT_FOR_GOLD-1),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, GeneralUserLevelUpgradePolicy.MIN_RECOMMEND_COUNT_FOR_GOLD),
                 new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE)
         );
     }
