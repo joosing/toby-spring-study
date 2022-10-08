e4e746a3b4bba6160aaed167b359a18add408dd2
Jsing <joosing711@gmail.com>
2022-10-08 17:52:51 +0900

    메서드 순서 조정

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -19,11 +19,6 @@ public class UserService {
         }
     }
 
-    private void upgradeLevel(User user) {
-        user.upgradeLevel(); // User 객체의 레벨을 업그레이드 하고,
-        userDao.update(user); // DB에 있는 User 정보를 업그레이드 합니다. 와 간결하다!!!
-    }
-
     private static boolean canUpgradeLevel(User user) {
         return switch (user.getLevel()) {
             case BASIC -> user.getLogin() >= MIN_LOGIN_COUNT_FOR_SILVER;
@@ -32,6 +27,11 @@ public class UserService {
             // default -> throw new IllegalArgumentException("Unknown Level: " + currentLevel);
             // 위 default 문은 새로운 Switch 문에서 불필요합니다. Enum 값이 추가되었는데 case 가 추가되지 않으면 컴파일 오류가 발생합니다.
         };
+    }
+
+    private void upgradeLevel(User user) {
+        user.upgradeLevel(); // User 객체의 레벨을 업그레이드 하고,
+        userDao.update(user); // DB에 있는 User 정보를 업그레이드 합니다. 와 간결하다!!!
     }
 
     public void add(User user) {
