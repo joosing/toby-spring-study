b3520e71ba042e2fba6d44897b57f5d00b469387
Jsing <joosing711@gmail.com>
2022-10-08 17:36:49 +0900

    Level Enum에서 레벨의 순서 처리

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -1,13 +1,23 @@
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Level {
-    BASIC(1), SILVER(2), GOLD(3);
+    // 아래 순서가 틀리면 Illegal forward Reference 오류가 발생합니다.
+    // 이는 객체가 선언되기 전에 사용되었기 때문입니다.
+    GOLD(3, null),
+    SILVER(2, GOLD),
+    BASIC(1, SILVER),
+    ;
+
     private final int level;
+    private final Level next;
 
     public int intValue() {
         return level;
+    }
+
+    public Level nextLevel() {
+        return next;
     }
 
     public static Level valueOf(int value) {
