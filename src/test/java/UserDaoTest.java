7a212ffd9cdda3454e76df350046c0f97174cbee
Jsing <joosing711@gmail.com>
2022-10-08 13:20:13 +0900

    User 필스처 이름 짧게 개선

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -8,7 +8,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
@@ -24,9 +23,9 @@ public class UserDaoTest {
     @Autowired
     private DataSource dataSource;
 
-    private User expectedUser1;
-    private User expectedUser2;
-    private User expectedUser3;
+    private User user1;
+    private User user2;
+    private User user3;
 
     @Before
     public void setUp() throws SQLException {
@@ -35,24 +34,24 @@ public class UserDaoTest {
         Assert.assertEquals(0, dao.getCount());
 
         // User Fixture 생성
-        expectedUser1 = new User("gyumee", "한주승", "hello", Level.BASIC, 1, 0);
-        expectedUser2 = new User("leegw700", "헤이", "hey", Level.SILVER, 55, 10);
-        expectedUser3 = new User("bumjin", "하이", "hi", Level.GOLD, 100, 40);
+        user1 = new User("gyumee", "한주승", "hello", Level.BASIC, 1, 0);
+        user2 = new User("leegw700", "헤이", "hey", Level.SILVER, 55, 10);
+        user3 = new User("bumjin", "하이", "hi", Level.GOLD, 100, 40);
     }
 
     @Test
     public void addAndGet() throws Exception {
-        dao.add(expectedUser1);
-        dao.add(expectedUser2);
+        dao.add(user1);
+        dao.add(user2);
         Assert.assertEquals(2, dao.getCount());
 
-        final User testUser = dao.get(expectedUser1.getId());
-        Assert.assertEquals(expectedUser1.getName(), testUser.getName());
-        Assert.assertEquals(expectedUser1.getId(), testUser.getId());
+        final User testUser = dao.get(user1.getId());
+        Assert.assertEquals(user1.getName(), testUser.getName());
+        Assert.assertEquals(user1.getId(), testUser.getId());
 
-        final User testUser2 = dao.get(expectedUser2.getId());
-        Assert.assertEquals(expectedUser2.getName(), testUser2.getName());
-        Assert.assertEquals(expectedUser2.getId(), testUser2.getId());
+        final User testUser2 = dao.get(user2.getId());
+        Assert.assertEquals(user2.getName(), testUser2.getName());
+        Assert.assertEquals(user2.getId(), testUser2.getId());
     }
 
     @Test
@@ -60,26 +59,26 @@ public class UserDaoTest {
         final List<User> users0 = dao.getAll();
         Assert.assertEquals(0, users0.size());
 
-        dao.add(expectedUser1);
+        dao.add(user1);
         final List<User> users1 = dao.getAll();
         Assert.assertEquals(1, users1.size());
-        checkSameUser(expectedUser1, users1.get(0));
+        checkSameUser(user1, users1.get(0));
 
-        dao.add(expectedUser2);
+        dao.add(user2);
         final List<User> users2 = dao.getAll();
         Assert.assertEquals(2, users2.size());
-        checkSameUser(expectedUser1, users2.get(0));
-        checkSameUser(expectedUser2, users2.get(1));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
 
-        dao.add(expectedUser3);
+        dao.add(user3);
         final List<User> users3 = dao.getAll();
         Assert.assertEquals(3, users3.size());
-        checkSameUser(expectedUser3, users3.get(0));
-        checkSameUser(expectedUser1, users3.get(1));
-        checkSameUser(expectedUser2, users3.get(2));
+        checkSameUser(user3, users3.get(0));
+        checkSameUser(user1, users3.get(1));
+        checkSameUser(user2, users3.get(2));
     }
 
-    private void checkSameUser(User user1, User user2) {
+    private static void checkSameUser(User user1, User user2) {
         Assert.assertEquals(user1.getId(), user2.getId());
         Assert.assertEquals(user1.getName(), user2.getName());
         Assert.assertEquals(user1.getPassword(), user2.getPassword());
@@ -90,13 +89,13 @@ public class UserDaoTest {
 
     @Test
     public void getCount() throws Exception {
-        dao.add(expectedUser1);
+        dao.add(user1);
         Assert.assertEquals(1, dao.getCount());
 
-        dao.add(expectedUser2);
+        dao.add(user2);
         Assert.assertEquals(2, dao.getCount());
 
-        dao.add(expectedUser3);
+        dao.add(user3);
         Assert.assertEquals(3, dao.getCount());
     }
 
@@ -109,8 +108,8 @@ public class UserDaoTest {
     public void duplicatedKey() {
         dao.deleteAll();
 
-        dao.add(expectedUser1);
-        dao.add(expectedUser1);
+        dao.add(user1);
+        dao.add(user1);
     }
 
     @Test
@@ -118,8 +117,8 @@ public class UserDaoTest {
         dao.deleteAll();
 
         try {
-            dao.add(expectedUser1);
-            dao.add(expectedUser1);
+            dao.add(user1);
+            dao.add(user1);
         } catch (DuplicateKeyException ex) {
             final SQLException sqlEx = (SQLException) ex.getRootCause();
             final SQLExceptionTranslator set = new SQLErrorCodeSQLExceptionTranslator(dataSource);
