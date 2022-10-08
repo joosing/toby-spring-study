bde350ded7c0e5e106614a298472bf8d25b297d0
Jsing <joosing711@gmail.com>
2022-10-08 14:28:37 +0900

    upgradeLevels() 테스트 코드

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -8,4 +14,38 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class UserServiceTest {
     @Autowired
     UserService userService;
+    @Autowired
+    UserDao userDao;
+
+    List<User> users; // 테스트 픽스처
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjini", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+        users.forEach(user -> userDao.add(user));
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        final User userUpdate = userDao.get(user.getId());
+        Assert.assertEquals(expectedLevel, userUpdate.getLevel());
+    }
 }
