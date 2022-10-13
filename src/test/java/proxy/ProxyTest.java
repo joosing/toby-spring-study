1ab77579085f635cd530956c2404e119fc9d865b
Jsing <joosing711@gmail.com>
2022-10-14 00:01:29 +0900

    style: 주석제거

--- src/test/java/proxy/ProxyTest.java
+++ src/test/java/proxy/ProxyTest.java
@@ -16,12 +16,6 @@ public class ProxyTest {
 
     @Test
     public void dynamicProxy() {
-        /*
-         * 궁금한점?
-         * 자바의 리플렉션 기술을 사용하면 Hello 라는 인터페이스 타입  클래스 객체만 가지고 프록시 객체를 만들어 낼 수 있는가?
-         * 다이나믹 프록시 객체는 타겟의 모든 메서드 객체를 가지고 있다고 대응되는 프록시 메서드 호출 시 InvocationHandler 로
-         * 메서드를 파라미터로 념겨주게 되는가?
-         */
         Hello hello = (Hello) Proxy.newProxyInstance(
                 getClass().getClassLoader(),
                 new Class[] { Hello.class },
