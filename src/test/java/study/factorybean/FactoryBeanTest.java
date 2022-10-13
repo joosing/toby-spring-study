5f8c0b3a2d0a610bce69619c8e7252e0dd53eb2f
Jsing <joosing711@gmail.com>
2022-10-14 01:12:07 +0900

    feat: 팩토리 빈 학습 테스트

---
+++ src/test/java/study/factorybean/FactoryBeanTest.java
@@ -0,0 +1,29 @@
+package study.factorybean;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) // 스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+@ContextConfiguration(locations="/factoryBeanTest.xml") // 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
+public class FactoryBeanTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void getMessageFromFactoryBean() {
+        Object message = context.getBean("message");
+        Assertions.assertSame(Message.class, message.getClass());
+        Assertions.assertEquals(((Message)message).getText(), "Factory Bean");
+    }
+
+    @Test
+    public void getFactoryBean() {
+        Object factory = context.getBean("&message");
+        Assertions.assertSame(MessageFactoryBean.class, factory.getClass());
+    }
+}
