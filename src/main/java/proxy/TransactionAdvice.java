127cfc97b2df1213a7f4fe7a36fed47a3fbcee27
Jsing <joosing711@gmail.com>
2022-10-15 16:52:03 +0900

    feat : 스프링 ProxyFactoryBean을 활용한 Transaction 부가기능 적용

---
+++ src/main/java/proxy/TransactionAdvice.java
@@ -0,0 +1,28 @@
+package proxy;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+public class TransactionAdvice implements MethodInterceptor {
+    private PlatformTransactionManager transactionManager;
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object ret = invocation.proceed();
+            transactionManager.commit(status);
+            return ret;
+        } catch (RuntimeException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
