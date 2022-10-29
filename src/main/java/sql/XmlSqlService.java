669d6f72ccb12f74f29cbe492eb791d72746154f
Jsing <joosing711@gmail.com>
2022-10-29 09:43:56 +0900

    feat: 전용 XML 파일을 이용한 SQL 서비스

---
+++ src/main/java/sql/XmlSqlService.java
@@ -0,0 +1,43 @@
+package sql;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import sql.jaxb.SqlType;
+import sql.jaxb.Sqlmap;
+
+public class XmlSqlService implements SqlService {
+    private final Map<String, String> sqlMap = new HashMap<>();
+
+    public XmlSqlService() throws JAXBException {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        System.out.println("contextPath = " + contextPath);
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = getClass().getResourceAsStream("/sqlmap.xml");
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlMap.put(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        String sql = sqlMap.get(key);
+        if (sql == null) {
+            throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다");
+        } else {
+            return sql;
+        }
+    }
+}
