d7388bf96817ddd4d6d0cf928a8a8d2f3f8937a6
Jsing <joosing711@gmail.com>
2022-10-29 16:27:47 +0900

    feat: 디폴트 의존관계를 갖는 빈

--- src/main/java/sql/JaxbXmlSqlReader.java
+++ src/main/java/sql/JaxbXmlSqlReader.java
@@ -11,7 +11,8 @@ import sql.jaxb.Sqlmap;
 
 @SuppressWarnings("DuplicatedCode")
 public class JaxbXmlSqlReader implements SqlReader {
-    private String sqlmapFile;
+    private static final String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+    private String sqlmapFile = DEFAULT_SQLMAP_FILE;
 
     public void setSqlmapFile(String sqlmapFile) {
         this.sqlmapFile = sqlmapFile;
