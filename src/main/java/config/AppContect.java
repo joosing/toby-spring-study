487c63a67490915e4b6dd91451ff17c7348212ce
Jsing <joosing711@gmail.com>
2022-11-06 09:23:22 +0900

    feat: SqlServiceContext 컨텍스트의 분리

--- src/main/java/config/AppContect.java
+++ src/main/java/config/AppContect.java
@@ -6,24 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import dao.UserDao;
-import sql.EmbeddedDbSqlRegistry;
-import sql.OxmSqlService;
-import sql.SqlRegistry;
-import sql.SqlService;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"dao", "service"})
+@Import(SqlServiceContext.class)
 public class AppContect {
     @Autowired
     UserDao userDao;
@@ -44,37 +38,5 @@ public class AppContect {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager(dataSource());
         transactionManager.setDataSource(dataSource());
         return transactionManager;
-    }
-
-    @Bean
-    public SqlService sqlService() {
-        OxmSqlService sqlService = new OxmSqlService();
-        sqlService.setUnmarshaller(unmarshaller());
-        sqlService.setSqlRegistry(sqlRegistry());
-        sqlService.loadSql();
-        return sqlService;
-    }
-
-    @Bean
-    public SqlRegistry sqlRegistry() {
-        EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
-        sqlRegistry.setDataSource(embeddedDatabase());
-        return sqlRegistry;
-    }
-
-    @Bean
-    public DataSource embeddedDatabase() {
-        return new EmbeddedDatabaseBuilder()
-                .setName("embeddedDatabase")
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:/sql/sqlRegistrySchema.sql")
-                .build();
-    }
-
-    @Bean
-    public Unmarshaller unmarshaller() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setContextPath("sql.jaxb");
-        return marshaller;
     }
 }
---
+++ src/main/java/config/SqlServiceContext.java
@@ -0,0 +1,51 @@
+package config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import sql.EmbeddedDbSqlRegistry;
+import sql.OxmSqlService;
+import sql.SqlRegistry;
+import sql.SqlService;
+
+@Configuration
+public class SqlServiceContext {
+
+    @Bean
+    public SqlService sqlService() {
+        OxmSqlService sqlService = new OxmSqlService();
+        sqlService.setUnmarshaller(unmarshaller());
+        sqlService.setSqlRegistry(sqlRegistry());
+        sqlService.loadSql();
+        return sqlService;
+    }
+
+    @Bean
+    public SqlRegistry sqlRegistry() {
+        EmbeddedDbSqlRegistry sqlRegistry = new EmbeddedDbSqlRegistry();
+        sqlRegistry.setDataSource(embeddedDatabase());
+        return sqlRegistry;
+    }
+
+    @Bean
+    public DataSource embeddedDatabase() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embeddedDatabase")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:/sql/sqlRegistrySchema.sql")
+                .build();
+    }
+
+    @Bean
+    public Unmarshaller unmarshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("sql.jaxb");
+        return marshaller;
+    }
+}
